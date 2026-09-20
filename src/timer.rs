1d2fea1c602afa602df360348217a6491348da08
Andrey Sverdlichenko <blaze@rusty.zone>
2026-09-19 21:10:10 -0400

    Export more timer structs

--- src/timer.rs
+++ src/timer.rs
@@ -5,4 +5,6 @@ mod timers;
 pub use lptim::{
     Disabled, Enabled, LowPowerTimer, LptimCounter, LptimEvent, LptimExt, LptimPrescaler,
 };
-pub use timers::{Channel1, Channel2, Channel3, Channel4, Pwm, PwmPin, Timer, TimerExt};
+pub use timers::{
+    BasicTimEvent, Channel1, Channel2, Channel3, Channel4, Counter, Pwm, PwmPin, Timer, TimerExt,
+};
